5cf33e69bccbd9d921a3be50fe415f6ca91fa58b
Mirecek2011CZ <mirek.hanc@seznam.cz>
2023-11-27 14:14:42 +0100

    first commit

---
+++ res/js/main.js
@@ -0,0 +1,11 @@
+const playing_board = [
+    null, 0, null, 1, null, 2, null, 3,
+    4, null, 5, null, 6, null, 7, null,
+    null, 8, null, 9, null, 10, null, 11,
+    null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null,
+    12, null, 13, null, 14, null, 15, null,
+    null, 16, null, 17, null, 18, null, 19,
+    20, null, 21, null, 22, null, 23, null
+]
+
