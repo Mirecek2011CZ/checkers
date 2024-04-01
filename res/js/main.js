806f5033a5a2cb2d0897ba7bf88a1c8329d1c31c
Mirecek2011CZ <mirek.hanc@seznam.cz>
2024-04-01 22:00:32 +0200

    Init

--- res/js/main.js
+++ res/js/main.js
@@ -1,30 +1,30 @@
 let board = document.querySelector(".checkerboard");
 
-let player1 = "\u2659"; 
-let player2 = "\u2658"; 
+let player1 = "\u2659";
+let player2 = "\u2658";
 
-let selected; 
-let index1 = -1; 
-let index2 = -1; 
-let player1total = 12; 
-let player2total = 12; 
-
+let selected;
+let index1 = -1;
+let index2 = -1;
+let player1total = 12;
+let player2total = 12;
 
 console.log(board.children[3].innerHTML);
 
 board.addEventListener("click", function (event) {
   let target = event.target;
-  
+
   if (target.innerHTML === player1 || target.innerHTML === player2) {
     selected = target.innerHTML;
     from = target;
     index1 = [...board.children].indexOf(target);
-  } 
-  else if (from && legalMove(from, target)) {
+  } else if (from && legalMove(from, target)) {
     index2 = [...board.children].indexOf(target);
     if (playerRules(selected, index1, index2)) {
       let total = index1 - index2;
-      console.log(`Player ${selected} from index ${index1} to index ${index2}, Difference: ${total}`);
+      console.log(
+        `Player ${selected} from index ${index1} to index ${index2}, Difference: ${total}`
+      );
 
       target.innerHTML = selected;
       from.innerHTML = "";
@@ -36,10 +36,12 @@ board.addEventListener("click", function (event) {
 });
 
 function legalMove(from, to) {
-  return to.innerHTML !== player1 && to.innerHTML !== player2 &&
-    to.classList.contains("white");
+  return (
+    to.innerHTML !== player1 &&
+    to.innerHTML !== player2 &&
+    to.classList.contains("white")
+  );
 }
-
 
 function playerRules(sel, from, to) {
   let moveDiff = from - to;
@@ -60,9 +62,7 @@ function playerRules(sel, from, to) {
         checkWinner(player1total, player2total);
       }
     }
-  } 
-
-  else if (sel == player2) {
+  } else if (sel == player2) {
     flag = to > from;
     if (flag) {
       console.log("Player 1's turn");
@@ -82,13 +82,10 @@ function playerRules(sel, from, to) {
 }
 
 function checkWinner(p1count, p2count) {
-
   if (p1count <= 0) {
     if (confirm("Player 1 won the game! Do you want to restart the game?")) {
     }
-  } 
-
-  else if (p2count <= 0) {
+  } else if (p2count <= 0) {
     if (confirm("Player 2 won the game! Do you want to restart the game?")) {
     }
   }
